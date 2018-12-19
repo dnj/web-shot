7114e31bb7188dcba4fe39b87921c155a7fb4b79
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2018-12-19 17:50:24 +0330

    Fix #1

--- src/HttpServer/Controllers/Capture.ts
+++ src/HttpServer/Controllers/Capture.ts
@@ -1,10 +1,16 @@
+import * as Jimp from "jimp";
+import {TimeoutError} from "puppeteer/Errors";
 import * as sharp from "sharp";
 import App from "../../App";
 import ServerError from "../../Library/Exceptions/ServerError";
 import Client from "../../Library/HttpServer/Client";
 import InputValidation, { InputType } from "../../Library/InputValidation";
 import Shot, { Format, Status } from "../../Models/Shot";
-
+enum ErrorType {
+	RESOLVE,
+	TIMEOUT,
+	INTERNAL,
+}
 interface IData {
 	url: string;
 	maxAge: number; // Default: 86400
@@ -64,9 +70,26 @@ export default class Capture {
 
 		}, client.url.query as any);
 		const data = validator.validate() as IData;
-		const shot = await Capture.shoot(data);
-		const image = await Capture.doResize(shot, data);
-		client.sendFile(image);
+		try {
+			const shot = await Capture.shoot(data);
+			const image = await Capture.doResize(shot, data);
+			client.sendFile(image);
+		} catch (e) {
+			if (e instanceof TimeoutError) {
+				Capture.sendErrorMessage(client, ErrorType.TIMEOUT, data.format, data.width, data.crop);
+			} else if (e instanceof Error) {
+				if (e.message.indexOf("net::ERR_NAME_NOT_RESOLVED") !== -1) {
+					Capture.sendErrorMessage(client, ErrorType.RESOLVE, data.format, data.width, data.crop);
+				} else if (e.message.indexOf("net::ERR_CONNECTION_TIMED_OUT") !== -1) {
+					Capture.sendErrorMessage(client, ErrorType.TIMEOUT, data.format, data.width, data.crop);
+				} else {
+					Capture.sendErrorMessage(client, ErrorType.INTERNAL, data.format, data.width, data.crop);
+				}
+			} else {
+				Capture.sendErrorMessage(client, ErrorType.INTERNAL, data.format, data.width, data.crop);
+				throw e;
+			}
+		}
 	}
 	private static async shoot(data: IData) {
 		let shot: Shot;
@@ -88,19 +111,26 @@ export default class Capture {
 		} while (!shot);
 		if (!shot.image) {
 			const page = await Capture.newPage();
-			await page.setViewport({width: data.viewportWidth, height: data.viewportHeight});
-			await page.goto(shot.url);
-			const file = Shot.getImageStoragePath() + "/" + shot.id + "." + data.format;
-			await page.screenshot({
-				path: file,
-				type: data.format === "png" ? "png" : "jpeg",
-				fullPage: data.fullPage === true,
-			});
-			page.close();
-			shot.set("status", Status.Available);
-			shot.set("image", shot.id + "." + data.format);
-			shot.set("capture_at", Math.floor(Date.now() / 1000));
-			shot.save();
+			try {
+				await page.setViewport({width: data.viewportWidth, height: data.viewportHeight});
+				await page.goto(shot.url);
+				const file = Shot.getImageStoragePath() + "/" + shot.id + "." + data.format;
+				await page.screenshot({
+					path: file,
+					type: data.format === "png" ? "png" : "jpeg",
+					fullPage: data.fullPage === true,
+				});
+				page.close();
+				shot.set("status", Status.Available);
+				shot.set("image", shot.id + "." + data.format);
+				shot.set("capture_at", Math.floor(Date.now() / 1000));
+				shot.save();
+			} catch (e) {
+				page.close();
+				shot.set("status", Status.Error);
+				shot.save();
+				throw e;
+			}
 		}
 		return shot;
 	}
@@ -148,5 +178,24 @@ export default class Capture {
 			await resizer.toFile(image);
 		}
 		return image;
+	}
+	private static async sendErrorMessage(client: Client, type: ErrorType, format: "jpg" | "png", width?: number, height?: number) {
+		const image = await Jimp.read(width, height, 0xffffffff);
+		const fontBig = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
+		const fontSmall = await Jimp.loadFont(Jimp.FONT_SANS_12_BLACK);
+		const marginTop = Math.floor(height / 3);
+		const marginLeft = Math.floor(width / 8);
+		if (type === ErrorType.RESOLVE) {
+			image.print(fontBig, marginLeft, marginTop, "DNS ERROR", marginLeft * 6, 20);
+			image.print(fontSmall, marginLeft, marginTop + 40, "The requested URL could not be retrieved", Math.floor(width / 8) * 6, Math.floor(height / 3) * 2);
+		} else if (type === ErrorType.TIMEOUT) {
+			image.print(fontBig, marginLeft, marginTop, "Connection Error", marginLeft * 6, 20);
+			image.print(fontSmall, marginLeft, marginTop + 40, "Connection timed out", Math.floor(width / 8) * 6, Math.floor(height / 3) * 2);
+		}  else if (type === ErrorType.INTERNAL) {
+			image.print(fontBig, marginLeft, marginTop, "Internal Error", marginLeft * 6, 20);
+			image.print(fontSmall, marginLeft, marginTop + 40, "The requested URL could not be retrieved", Math.floor(width / 8) * 6, Math.floor(height / 3) * 2);
+		}
+		const mime = format === "jpg" ? Jimp.MIME_JPEG : Jimp.MIME_PNG;
+		client.send(await image.getBufferAsync(mime));
 	}
 }
