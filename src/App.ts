e9aefdf3257e975734a980bb1cef65a3e668030b
Hossein Hosni <hosni.hossein@gmail.com>
2021-05-09 00:17:21 +0430

    #5 Add signal manager and dotenv

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,6 @@
 import * as fs from "fs";
+import * as os from "os";
+import * as dotenv from "dotenv";
 import * as puppeteer from "puppeteer";
 import HttpRouting from "./HttpServer/Routing";
 import ConfigManager from "./Library/ConfigManager";
@@ -8,13 +10,54 @@ import Shot from "./Models/Shot";
 
 export default class App {
 	public static async run() {
+		if (process.argv.length > 2) {
+			const command = process.argv[2];
+			switch (command) {
+				case "reload":
+					this.sendReloadSignal();
+					break;
+				default:
+					console.log(`webshot: '${command}' is not a webshot command.`);
+				break;
+			}
+			process.exit();
+		}
+
+		App.savePID();
+		App.listenForSignals();
 		App.CWD();
+		App.initDotENV();
 		App.runDB();
 		await App.loadConfig();
 		await App.runHttpServer();
 		await App.changeUserGroup();
 		App.removeOldShotsInterval();
 		App.runBrowser();
+	}
+	public static reload() {
+		App.config.clearCache();
+		App.config.preload();
+		if (App.httpServer) {
+			App.httpServer.stop();
+			App.httpServer.run();
+		}
+	}
+	public static sendReloadSignal() {
+		if (App.isDeamonIsRunnig()) {
+			const PID = App.getPID();
+			console.log(`webshot: send 'SIGHUP' signal to PID: '${PID}'`);
+			try {
+				process.kill(PID, os.constants.signals.SIGHUP);
+			} catch (err) {
+				console.log("webshot: error in send reload signal.\n", err);
+				if (err.errno === os.constants.errno.ESRCH) {
+					App.removePID();
+				}
+			}
+		} else {
+			console.log("webshot: webshot is not running!");
+			App.removePID();
+		}
 	}
 	public static getDatabaseManager() {
 		return this.databaseManager;
@@ -58,28 +101,96 @@ export default class App {
 			}
 		}
 	}
+	public static isDeamonIsRunnig(): boolean {
+		const PID = App.getPID();
+		if (PID) {
+			try {
+				/** check has process with PID */
+				process.kill(PID, 0);
+			} catch (err) {
+				App.removePID();
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	protected static listenForSignals() {
+		process.on("SIGTERM", () => {
+			console.log("webshot: got SIGTERM signal.");
+			if (App.databaseManager) {
+				App.databaseManager.close();
+			}
+			App.removePID();
+			process.exit();
+		});
+		process.on("SIGHUP", () => {
+			console.log("webshot: got SIGHUP signal, reload 'webshot' deamon.");
+			App.reload();
+		});
+	}
+	protected static getPID(): number | null {
+		if (fs.existsSync(App.PID_FILE)) {
+			return parseInt(fs.readFileSync(App.PID_FILE, {
+				encoding: "UTF8",
+			}), 10);
+		}
+		return null;
+	}
+	private static savePID() {
+		fs.writeFileSync(App.PID_FILE, process.pid);
+	}
+	private static removePID() {
+		if (fs.existsSync(App.PID_FILE)) {
+			fs.unlink(App.PID_FILE, (err) => {
+				console.error(`webshot: can not remove PID file located in: '${App.PID_FILE}'`);
+			});
+		}
+	}
+	private static readonly PID_FILE = "/run/web-shot.pid";
 	private static databaseManager: DatabaseManager;
+	private static httpServer: HttpServer;
 	private static browser: puppeteer.Browser;
 	private static config: ConfigManager;
 	private static browserIsLocked = false;
 	private static browserUnLockPromises: Array<{resolve: () => void, lock: boolean}> = [];
+	private static initDotENV() {
+		dotenv.config({
+			path: "../.env",
+		});
+	}
 	private static runDB() {
 		App.databaseManager = new DatabaseManager({
-			host: "127.0.0.1",
-			username: "YOUR_USER",
-			password: "PASSWORD",
-			database: "DATABASE_NAME",
-			charset: "utf8mb4",
+			host: process.env.DB_HOST || "127.0.0.1",
+			username: process.env.WEBSHOT_DB_USERNAME || "YOUR_USER",
+			password: process.env.WEBSHOT_DB_PASSWORD || "PASSWORD",
+			database: process.env.WEBSHOT_DB_NAME ||"DATABASE_NAME",
+			charset: process.env.WEBSHOT_DB_CHARSET || "utf8mb4",
 		});
 	}
 	private static async runHttpServer() {
-		const ssl = await App.getConfig().get("https") as ISSLOptions;
-		const server = new HttpServer(HttpRouting, {
-			port: await App.getConfig().get("http_port", 80) as number,
-			hostname: await App.getConfig().get("http_hostname") as string,
+		const port: number = process.env.WEBSHOT_PORT ?
+			parseInt(process.env.WEBSHOT_PORT, 10) :
+			await App.getConfig().get("http_port", 80) as number;
+
+		const hostname: string = process.env.WEBSHOT_HOSTNAME ||
+			await App.getConfig().get("http_hostname") as string;
+
+		const ssl: ISSLOptions = (process.env.WEBSHOT_SSL_CERT_PATH && process.env.WEBSHOT_SSL_KEY_PATH) ? {
+			cert: process.env.WEBSHOT_SSL_CERT_PATH,
+			key: process.env.WEBSHOT_SSL_KEY_PATH,
+			port: process.env.WEBSHOT_SSL_PORT ? parseInt(process.env.WEBSHOT_SSL_PORT) : 443,
+			redirect: [1, "1", "true", "yes", "on"].indexOf(process.env.WEBSHOT_SSL_REDIRECT) > -1,
+		} : await App.getConfig().get("https") as ISSLOptions;
+
+		const config = {
+			port: port,
+			hostname: hostname,
 			ssl: ssl,
-		});
-		await server.run();
+		};
+		console.log("App::runHttpServer", config);
+		App.httpServer = new HttpServer(HttpRouting, config);
+		await App.httpServer.run();
 	}
 	private static async runBrowser() {
 		App.browser = await puppeteer.launch({
